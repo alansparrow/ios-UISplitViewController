57e6042c0bf22e9830b9497bc852f5e30702c6e8
Alan Sparrow <trannguyenbaotrung@gmail.com>
2014-01-01 19:22:40 +0900

    Init

---
+++ ChannelViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChannelViewController.h
+//  Nerdfeed
+//
+//  Created by Alan Sparrow on 1/1/14.
+//  Copyright (c) 2014 Alan Sparrow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChannelViewController : NSObject
+
+@end
---
+++ ChannelViewController.m
@@ -0,0 +1,13 @@
+//
+//  ChannelViewController.m
+//  Nerdfeed
+//
+//  Created by Alan Sparrow on 1/1/14.
+//  Copyright (c) 2014 Alan Sparrow. All rights reserved.
+//
+
+#import "ChannelViewController.h"
+
+@implementation ChannelViewController
+
+@end
